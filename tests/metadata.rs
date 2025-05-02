3c445c30c7ceea5b5515950d632372c4d0e5d86a
Brian Langenberger <bjl@usa.net>
2025-05-02 14:09:52 -0500

    Add some metadata block roundtrip tests

--- tests/metadata.rs
+++ tests/metadata.rs
@@ -3,6 +3,31 @@ use flac_codec::metadata::{
     Block, Cuesheet, CuesheetTrack, VorbisComment, read_blocks, write_blocks,
 };
 
+fn roundtrip_test(flac: &[u8]) {
+    use std::io::Read;
+
+    let mut data = std::io::Cursor::new(flac);
+    let blocks = dbg!(
+        read_blocks(data.by_ref())
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap()
+    );
+    let mut output = Vec::new();
+    write_blocks(blocks, &mut output).unwrap();
+    std::io::copy(&mut data, &mut output).unwrap();
+    assert_eq!(flac.len(), output.len());
+    assert_eq!(flac, output.as_slice());
+}
+
+#[test]
+fn test_block_roundtrips() {
+    roundtrip_test(include_bytes!("data/all-frames.flac").as_slice());
+    roundtrip_test(include_bytes!("data/seektable.flac").as_slice());
+    roundtrip_test(include_bytes!("data/cuesheet.flac").as_slice());
+    roundtrip_test(include_bytes!("data/comment.flac").as_slice());
+    roundtrip_test(include_bytes!("data/picture.flac").as_slice());
+}
+
 fn perform_test(flac: &[u8], f: impl FnOnce(&mut Vec<Block>)) -> Result<(), Error> {
     let mut blocks = read_blocks(flac).collect::<Result<Vec<_>, _>>().unwrap();
 
