24bda5b60fd6adeca55355f0ef6d4f5bb763f232
Brian Langenberger <bjl@usa.net>
2025-05-16 07:58:38 -0500

    Overhaul how Frame sample walking is implemented
    Using a multi-zip variant and flattening the result,
    I can build an iter_mut() implementation that walks over
    the Frame's samples in interleaved order.

--- src/audio.rs
+++ src/audio.rs
@@ -1,3 +1,15 @@
+// Copyright 2025 Brian Langenberger
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! For handling PCM audio in frame-sized chunks
+
+use smallvec::SmallVec;
+
 #[derive(Default)]
 pub struct Frame {
     // all samples, stacked by channel
@@ -31,7 +43,12 @@ impl Frame {
         self.channel_len
     }
 
-    pub fn resize(&mut self, bits_per_sample: u32, channels: usize, block_size: usize) -> &mut [i32] {
+    pub fn resize(
+        &mut self,
+        bits_per_sample: u32,
+        channels: usize,
+        block_size: usize,
+    ) -> &mut [i32] {
         self.bits_per_sample = bits_per_sample;
         self.channels = channels;
         self.channel_len = block_size;
@@ -73,12 +90,20 @@ impl Frame {
     }
 
     /// Iterates over any samples in interleaved order
+    #[inline]
     pub fn iter(&self) -> impl Iterator<Item = i32> {
-        // TODO - use MultiZip
-        (0..self.samples.len()).map(|i| {
-            let (sample, channel) = (i / self.channels, i % self.channels);
-            self.samples[channel * self.channel_len + sample]
-        })
+        MultiZip {
+            iters: self.channels().map(|c| c.iter().copied()).collect(),
+        }
+        .flatten()
+    }
+
+    #[inline]
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut i32> {
+        MultiZip {
+            iters: self.channels_mut().map(|c| c.iter_mut()).collect(),
+        }
+        .flatten()
     }
 
     /// Fills buffer with our samples in the given endianness
@@ -116,70 +141,47 @@ impl Frame {
         self.samples.chunks_exact(self.channel_len)
     }
 
+    /// Iterates over all channels as mutable samples
+    #[inline]
+    pub fn channels_mut(&mut self) -> impl Iterator<Item = &mut [i32]> {
+        self.samples.chunks_exact_mut(self.channel_len)
+    }
+
     /// Fills frame samples from bytes of the given endianness
     pub fn fill_from_buf<E: crate::byteorder::Endianness>(&mut self, buf: &[u8]) -> &Self {
-        fn buf_chunks<const BYTES_PER_SAMPLE: usize>(
-            channels: usize,
-            channel_len: usize,
-            buf: &[u8],
-        ) -> impl Iterator<Item = [u8; BYTES_PER_SAMPLE]> {
-            (0..channels).flat_map(move |c| {
-                (0..channel_len).map(move |s| {
-                    buf[((s * channels) + c) * BYTES_PER_SAMPLE
-                        ..((s * channels) + c + 1) * BYTES_PER_SAMPLE]
-                        .try_into()
-                        .unwrap()
-                })
-            })
-        }
+        // TODO - use array_chunks once that stabilizes
 
         match self.bytes_per_sample() {
             1 => {
                 self.channel_len = buf.len() / self.channels;
                 self.samples.resize(buf.len(), 0);
 
-                for (sample, bytes) in self.samples.iter_mut().zip(buf_chunks::<1>(
-                    self.channels,
-                    self.channel_len,
-                    buf,
-                )) {
-                    *sample = E::bytes_to_i8(bytes) as i32
+                for (sample, bytes) in self.iter_mut().zip(buf.chunks_exact(1)) {
+                    *sample = E::bytes_to_i8(bytes.try_into().unwrap()) as i32
                 }
             }
             2 => {
                 self.channel_len = (buf.len() / 2) / self.channels;
                 self.samples.resize(buf.len() / 2, 0);
 
-                for (sample, bytes) in self.samples.iter_mut().zip(buf_chunks::<2>(
-                    self.channels,
-                    self.channel_len,
-                    buf,
-                )) {
-                    *sample = E::bytes_to_i16(bytes) as i32
+                for (sample, bytes) in self.iter_mut().zip(buf.chunks_exact(2)) {
+                    *sample = E::bytes_to_i16(bytes.try_into().unwrap()) as i32
                 }
             }
             3 => {
                 self.channel_len = (buf.len() / 3) / self.channels;
                 self.samples.resize(buf.len() / 3, 0);
 
-                for (sample, bytes) in self.samples.iter_mut().zip(buf_chunks::<3>(
-                    self.channels,
-                    self.channel_len,
-                    buf,
-                )) {
-                    *sample = E::bytes_to_i24(bytes)
+                for (sample, bytes) in self.iter_mut().zip(buf.chunks_exact(3)) {
+                    *sample = E::bytes_to_i24(bytes.try_into().unwrap())
                 }
             }
             4 => {
                 self.channel_len = (buf.len() / 4) / self.channels;
                 self.samples.resize(buf.len() / 4, 0);
 
-                for (sample, bytes) in self.samples.iter_mut().zip(buf_chunks::<4>(
-                    self.channels,
-                    self.channel_len,
-                    buf,
-                )) {
-                    *sample = E::bytes_to_i32(bytes)
+                for (sample, bytes) in self.iter_mut().zip(buf.chunks_exact(4)) {
+                    *sample = E::bytes_to_i32(bytes.try_into().unwrap())
                 }
             }
             _ => panic!("unsupported number of bytes per sample"),
@@ -190,27 +192,28 @@ impl Frame {
 
     /// Fills frame samples from interleaved samples
     pub fn fill_from_samples(&mut self, samples: &[i32]) -> &Self {
-        fn samples_iter(
-            channels: usize,
-            channel_len: usize,
-            samples: &[i32],
-        ) -> impl Iterator<Item = i32> {
-            (0..channels)
-                .flat_map(move |c| (0..channel_len).map(move |s| samples[(s * channels) + c]))
-        }
-
         self.channel_len = samples.len() / self.channels;
         self.samples.resize(samples.len(), 0);
 
-        for (o, i) in
-            self.samples
-                .iter_mut()
-                .zip(samples_iter(self.channels, self.channel_len, samples))
-        {
-            *o = i;
+        for (o, i) in self.iter_mut().zip(samples) {
+            *o = *i;
         }
 
         self
     }
 }
 
+const MAX_CHANNELS: usize = 8;
+
+struct MultiZip<I> {
+    iters: SmallVec<[I; MAX_CHANNELS]>,
+}
+
+impl<I: Iterator> Iterator for MultiZip<I> {
+    type Item = SmallVec<[I::Item; MAX_CHANNELS]>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iters.iter_mut().map(|i| i.next()).collect()
+    }
+}
